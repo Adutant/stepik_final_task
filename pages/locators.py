a8b8df8620d4cc019553d08a8a4ba7aa2e4eb2a1
Adeliia Kadyrova <a.kadyrova@payler.com>
2025-03-21 16:05:39 +0100

    added test for adding product to basket with offer

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,10 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_ADDED_TO_BASKET_MESSAGE = (By.XPATH, "//div[@id='messages']/div[1]/div/strong")
+    PRODUCT_PRICE_MESSAGE = (By.XPATH, "//div[@id='messages']/div[3]/div/p/strong")
+    PRODUCT_NAME = (By.XPATH, "//article[@class='product_page']//h1")
+    PRODUCT_PRICE = (By.XPATH, "//article[@class='product_page']//p[@class='price_color']")
---
+++ pages/product_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_to_basket_button.click()
+
+    def should_be_message_product_add_to_basket_shown(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+        product_added_to_basket_message = (
+            self.browser.find_element(*ProductPageLocators.PRODUCT_ADDED_TO_BASKET_MESSAGE))
+        assert product_name.text == product_added_to_basket_message.text, \
+            "Product name in basket isn't the expected one"
+
+
+    def should_be_message_product_price_shown(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        product_price_message = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_MESSAGE)
+        assert product_price.text == product_price_message.text, "Product price in basket isn't the expected one"
+
