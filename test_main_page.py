0ec5fb3ccb9042b575b1a74e79170875053cd1fc
Adeliia Kadyrova <a.kadyrova@payler.com>
2025-03-21 13:26:38 +0100

    fix of the login test

--- test_main_page.py
+++ test_main_page.py
@@ -8,7 +8,7 @@ def test_guest_can_go_to_login_page(browser):
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
-    login_page = LoginPage(browser, link)
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
 
